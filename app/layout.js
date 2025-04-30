03c307c82e793b601a7c43920a692a6fc2d2b93f
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-04-30 16:11:32 +0600

    added banner section

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${outfitFOnt.className} ${ovoFOnt.className} antialiased`}
+        className={`${outfitFOnt.className} ${ovoFOnt.className} antialiased leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
